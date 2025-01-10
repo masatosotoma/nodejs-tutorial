f8aaa763670ee2305cbf91e04381265bc5cd4a99
MasatoSotoma <masatosotoma@gmail.com>
2025-01-10 08:38:41 -0500

    Setup the server and trid it in the local port

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("nodejs");
+const http = require("http");
+const PORT = 8000;
+
+// create webserver
+
+const server = http.createServer((req, res) => {
+  //access coming from browser
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello World</h1>");
+  res.end();
+});
+
+server.listen(PORT, () => {
+  console.log("server running!");
+});
