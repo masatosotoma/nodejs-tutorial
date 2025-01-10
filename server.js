1161d35342e892ae21f6d1b7181231309ea5cd50
MasatoSotoma <masatosotoma@gmail.com>
2025-01-10 08:42:47 -0500

    Created index file and put it in server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const http = require("http");
 const PORT = 8000;
+const html = require("fs").readFileSync("./index.html");
 
 // create webserver
 
 const server = http.createServer((req, res) => {
   //access coming from browser
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<h1>Hello World</h1>");
+  res.write(html);
   res.end();
 });
 
